5955340d0739823a4e768ec45597f0511e4a6f11
Andrey <93659653+Bartennder@users.noreply.github.com>
2022-08-08 20:54:56 +0300

    Update telegram.php

--- telegram.php
+++ telegram.php
@@ -17,7 +17,7 @@ foreach($arr as $key => $value) {
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-if ($sendToTelegram && $sendToTelegram2) {
+if ($sendToTelegram) {
   header('Location: thanks.html');
 } else {
   echo "Error";
