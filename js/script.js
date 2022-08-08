96d65ab35647dba7aaf1e92f2d96f446879e3050
Andrey <93659653+Bartennder@users.noreply.github.com>
2022-08-08 20:37:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+
+$(document).ready(function(){
+    $('[data-modal=thanks1]').on('click', function() {
+        $('.overlay').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay').fadeOut('slow');
+    });
+});
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+const hamburger = document.querySelector('.hamburger'),
+    overlay = document.querySelector('.menu__overlay'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
---
+++ telegram.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$text = $_POST['text'];
+$token = "5315207825:AAHy7pYEqXSuD54MFSB6uqAmjhJbWhnJu2A";
+$chat_id = "-71884902";
+$arr = array(
+  'Имя: ' => $name,
+  'Телефон: ' => $phone,
+  'Сообщение:' => $text
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram && $sendToTelegram2) {
+  header('Location: thanks.html');
+} else {
+  echo "Error";
+}
+?>
